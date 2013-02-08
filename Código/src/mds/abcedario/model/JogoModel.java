4914e61d729845ae69726f3bb3cec66abc9e4aaf
lucaskanashiro <lucas-kanashiro@hotmail.com>
2013-02-08 17:51:41 -0200

    Classe modelo do modo de jogo

---
+++ Código/src/mds/abcedario/model/JogoModel.java
@@ -0,0 +1,32 @@
+package fga.mds.abcdario.model;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class JogoModel implements Serializable{
+
+	private int imagem;
+	private int botao;
+	
+	public JogoModel(int imagem, int botao){
+		setImagem(imagem);
+		setBotao(botao);
+	}
+
+	public int getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(int imagem) {
+		this.imagem = imagem;
+	}
+
+	public int getBotao() {
+		return botao;
+	}
+
+	public void setBotao(int botao) {
+		this.botao = botao;
+	}
+	
+}
