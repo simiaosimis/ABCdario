71b68af31d1e740ee46897a983f233a4483acf77
Luiza Schaidt <luiza.schaidt@gmail.com>
2013-02-24 23:00:36 -0300

    inicio

--- ABCDario/src/fga/mds/abcdario/view/InicioActivity.java
+++ ABCDario/src/fga/mds/abcdario/view/InicioActivity.java
@@ -1,11 +1,10 @@
 package fga.mds.abcdario.view;
 
 import fga.mds.abcdario.R;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,26 +13,30 @@ public class InicioActivity extends Activity {
 
 	private ImageView bt_iniciar, bt_sair, bt_sobre;
 	final Context context = this;
-	
+	//private MediaPlayer musica;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.inicio);
 		
-		inicializarComponentes(); 
+		inicializarComponentes();
 		definirEventos();
 	}
 		public void inicializarComponentes(){
 			bt_iniciar = (ImageView) findViewById(R.imgV.bt_iniciar);
 			bt_sair = (ImageView) findViewById(R.imgV.bt_sair);
 			bt_sobre = (ImageView) findViewById(R.imgV.bt_sobre);
+			
+			//musica = MediaPlayer.create(this, R.raw.musica_tela_inicial);
+			//musica.start();
 		}
 		
 		public void definirEventos(){
 		bt_iniciar.setOnClickListener(new View.OnClickListener(){			
 			
 			
-			public void onClick(View arg0) {			 
+			public void onClick(View arg0) {	
+				//musica.stop();
 				startActivity(new Intent(InicioActivity.this, Escolha.class));
 						 
 			}
@@ -41,42 +44,21 @@ public class InicioActivity extends Activity {
 		
 		bt_sair.setOnClickListener(new View.OnClickListener(){
 			
-			public void onClick(View arg0) {			 
-				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-						context);
-
-					
-					alertDialogBuilder.setTitle("        TEM CERTEZA QUE DESEJA SAIR?");
-					alertDialogBuilder		
-					
-						.setIcon(R.drawable.icone)		
-						.setCancelable(false)
-						.setNegativeButton("Não",new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog,int id) {
-								
-								dialog.cancel();
-							}
-						})
-						.setPositiveButton("Sim",new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog,int id) {
-								
-								System.exit(0);
-							}
-						});
-					AlertDialog popUpInformativo = alertDialogBuilder.create();
-					
-					popUpInformativo.show();
-					}
-				});
-
+			public void onClick(View arg0) {
+				//musica.stop();
+				finish();
+			}
+		});
+		
 
 		bt_sobre.setOnClickListener(new View.OnClickListener(){			
 			
-					public void onClick(View arg0) {			 
-						startActivity(new Intent(InicioActivity.this, Sobre.class));
-								 
-					}
-				});
-	
-			}
-	}
+			public void onClick(View arg0) {	
+				//musica.stop();
+				startActivity(new Intent(InicioActivity.this, Informacao.class));
+				}
+			});
+
+}
+
+}
