797fb542b49da6284d6efce2fa18a2da0fe81605
Luiza Schaidt <luiza.schaidt@gmail.com>
2013-02-26 00:56:12 -0300

    Aprender Controller

--- ABCDario/src/fga/mds/abcdario/control/AprenderController.java
+++ ABCDario/src/fga/mds/abcdario/control/AprenderController.java
@@ -1,5 +1,7 @@
 package fga.mds.abcdario.control;
 
+import java.util.ArrayList;
+
 import fga.mds.abcdario.model.AprenderModel;
 import fga.mds.abcdario.persistencia.AprenderPersistencia;
 
@@ -9,8 +11,9 @@ public class AprenderController {
 	
 	private AprenderPersistencia acessar = AprenderPersistencia.getInstance();
 	private AprenderModel tela;
+	private ArrayList<Integer> cadastroPosicao= new ArrayList<Integer>();
 
-	public int[] definirLetra(String letra){
+	public int[] definirLetra(int letra){
 		
 		int id[] = new int[2];
 		
@@ -22,7 +25,7 @@ public class AprenderController {
 		return id;
 	}
 
-	public int[] definirNumero(String numero){
+	public int[] definirNumero(int numero){
 
 		int id[] = new int[2];
 		
@@ -34,13 +37,74 @@ public class AprenderController {
 		return id;
 	}
 	
-	public char proximaLetra(char letra){
-		//implementar
-		return 'a';
+	public int definirAcaoLetra(int posicao, int acao) {
+		
+		if (acao == 0){
+			return posicao;
+		}
+		
+		if (acao == 1){
+			if(posicao == 25){
+				posicao = 0;
+				return posicao;
+			}else{
+				posicao++;
+				return posicao;
+			}
+			
+		}
+		if (acao == 2){
+			if(posicao == 0){
+				posicao = 25;
+				return posicao;
+			}else{
+				posicao--;
+				return posicao;
+			}
+			
+		}
+		return 0;
 	}
 	
-	public char anteriorLetra(char letra){
-		//implementar
-		return 'a';
+	public int definirAcaoNumero(int posicao, int acao) {
+		
+		if (acao == 0){
+			return posicao;
+		}
+		
+		if (acao == 1){
+			if(posicao == 8){
+				posicao = 0;
+				return posicao;
+			}else{
+				posicao++;
+				return posicao;
+			}
+			
+		}
+		if (acao == 2){
+			if(posicao == 0){
+				posicao = 8;
+				return posicao;
+			}else{
+				posicao--;
+				return posicao;
+			}
+			
+		}
+		return 0;
+	}
+
+	public void criandoPosicao(int posicao) {
+		cadastroPosicao.add(posicao);		
+	}
+	
+	public void alterandoPosicao(int posicao) {
+		cadastroPosicao.set(0, posicao);		
+	}
+	
+	public int retornandoPosicao() {
+		
+		return cadastroPosicao.get(0).intValue();		
 	}
 }
